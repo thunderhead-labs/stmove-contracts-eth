7c30a3ddb4c9e46913252242e8bf5c53115d03ec
Pierre <pierre@thunderhead.xyz>
2024-11-22 18:23:38 -0600

    movement-lab deps

--- src/Lock.sol
+++ src/Lock.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "movement/protocol-units/bridge/contracts/src/NativeBridge.sol";
 
 contract Lock is Initializable, OwnableUpgradeable{
 	IERC20 public fstMOVE;
